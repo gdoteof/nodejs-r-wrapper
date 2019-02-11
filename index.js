ad0cd6264089822af09de101ce73745e0898af16
Ubuntu <ghost@r.mooreserver>
2019-02-11 19:54:00 +0000

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 
 var express = require('express')
+var fileUpload = require('express-fileupload');
 var app = express()
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -9,11 +10,38 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+var multer  = require('multer')
+var upload = multer({ dest: '/home/ubuntu/upload_data' })
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hi tyler')
   console.log("root loaded");
 })
+
+app.post('/dee', upload.single('sampleFile'), function(req, res) {
+
+	console.log(req.file);
+  email = req.body.email;
+  filename = "/upload/" + req.file.filename;
+  argstring = ' "' + email + '" "' + filename + '"';
+  res.send('submission received');
+  const { exec } = require('child_process');
+  exec('sudo docker run -i --rm -v /home/ubuntu/rcode:/code -v /home/ubuntu/upload_data:/upload rserver Rscript /code/deedive.R '+ argstring, {maxBuffer : 500 * 1024}, (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err)
+  } else {
+   // the *entire* stdout and stderr (buffered)
+//   console.log(`stdout: ${stdout}`);
+   console.log(`stderr: ${stderr}`);
+  }
+  });
+  email = req.body.email;
+  argstring = '"' + email + '"';
+
+
+});
 
 app.post('/B', function(req, res) {
   artist = req.body.artist;
@@ -31,6 +59,7 @@ app.post('/B', function(req, res) {
    console.log(`stderr: ${stderr}`);
   }
 });
+    res.set('Access-Control-Allow-Origin: *');
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
